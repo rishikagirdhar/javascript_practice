a6519b913036d0111dd575d73567895103c54010
rishikagirdhar <145919332+rishikagirdhar@users.noreply.github.com>
2025-07-25 04:42:30 +0000

    datatype conversion

---
+++ 01Basics/03ConversionOperations.js
@@ -0,0 +1,37 @@
+let age = 33
+
+console.log(typeof age)
+
+let score="30"
+
+console.log(typeof score)
+
+let valInNum= Number(score)
+
+console.log(typeof(valInNum))
+
+let prob="20a"
+
+let valInProb= Number(prob)
+
+console.log(valInProb) // in case of converting null- converts to 0 and in case of converting undefined or string the new val will be NaN but detetype will be number in each of the case
+
+// in case of converson to Boolean- 0 or empty string is false whereas 1 or non empty string is true 
+
+// while converting to String 
+
+// OPERATIONS
+
+let value = 3
+let nval = -value
+console.log(nval)
+
+// arithematic operators
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true+true)
+
